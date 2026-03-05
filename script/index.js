bbf338da76d7eda7687fd128d24428b4c4fc07d1
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-06 02:24:08 +0600

    lesson 1st part done

---
+++ script/index.js
@@ -0,0 +1,33 @@
+const loadLessons =() => {
+    fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response
+    .then((res) => res.json()) // promise of json data
+    .then((json) => displayLesson(json.data))
+};
+
+const displayLesson = (lessons) =>{
+    //console.log(lessons);
+    //1. get the container & empty
+    const levelContsiner = document.getElementById('level-contsiner')
+    levelContsiner.innerHTML = "";
+
+    //2. get into every lessons
+    for( let lesson of lessons){
+        //3. create element
+
+        console.log(lesson)
+        const btnDiv = document.createElement('div')
+        btnDiv.innerHTML = `
+            <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson -${lesson.level_no} </button>
+        
+        `
+                    
+
+        // 4. append into container
+        levelContsiner.append(btnDiv)            
+
+    };
+        
+        
+
+}
+loadLessons();
